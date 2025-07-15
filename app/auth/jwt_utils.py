264e7d6e929d986f4148ed58fd7bf287be15631a
rompu <rom.puh@gmail.com>
2025-07-15 22:42:07 +0300

    add setting for black and make reformats

--- app/auth/jwt_utils.py
+++ app/auth/jwt_utils.py
@@ -11,9 +11,13 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+def create_access_token(
+    data: dict, expires_delta: Optional[timedelta] = None
+) -> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now(timezone.utc) + (
+        expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
@@ -25,6 +29,14 @@ def verify_access_token(token: str) -> dict:
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Недійсний токен або термін дії вичерпано.",
+            detail="Invalid token or expired.",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise Exception("Could not validate credentials")
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -12,10 +12,14 @@ router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @router.post("/register", response_model=Token)
-async def register(user_data: UserCreate, db: AsyncSession = Depends(get_async_session)):
+async def register(
+    user_data: UserCreate, db: AsyncSession = Depends(get_async_session)
+):
     existing_user = await get_user_by_email(db, user_data.email)
     if existing_user:
-        raise HTTPException(status_code=400, detail="User with this email exists")
+        raise HTTPException(
+            status_code=400, detail="User with this email exists"
+        )
 
     hashed_pw = hash_password(user_data.password)
     user = await create_user(db, user_data, hashed_pw)
@@ -24,10 +28,14 @@ async def register(user_data: UserCreate, db: AsyncSession = Depends(get_async_s
 
 
 @router.post("/login", response_model=Token)
-async def login(user_data: UserLogin, db: AsyncSession = Depends(get_async_session)):
+async def login(
+    user_data: UserLogin, db: AsyncSession = Depends(get_async_session)
+):
     user = await get_user_by_email(db, user_data.email)
     if not user or not verify_password(user_data.password, user.password_hash):
-        raise HTTPException(status_code=401, detail="Incorrect email or password")
+        raise HTTPException(
+            status_code=401, detail="Incorrect email or password"
+        )
 
     access_token = create_access_token({"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
